4dbe8e4f2801320e61389ec08e36f382ab1a4406
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-01-03 20:52:54 +0900

    add:  ユーザー認証

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  #devise_for :users
+
+  root 'welcome#index' #これを追記 
+
+  devise_scope :user do
+    get "user/:id", :to => "welcome#index"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
