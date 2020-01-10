639ab3200f8a8b34ac444ffb6b888c7e81aa3071
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-01-11 00:02:21 +0900

    update: display username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,9 @@ class ApplicationController < ActionController::Base
   
     # 入力フォームからアカウント名情報をDBに保存するために追加
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    end
-    
+      added_attrs = [ :name, :email, :password, :password_confirmation　]
+      devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+      devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+      devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+    end    
 end
