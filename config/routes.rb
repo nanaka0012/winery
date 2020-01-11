d2abb972aa0603dc819206f33d6e3571b9f44534
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-01-11 12:25:37 +0900

    add: C&V mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,6 @@
-# Rails.application.routes.draw do
-#   devise_for :users
-#   #devise_for :users
-
-
-#   devise_scope :user do
-#     get "user/:id", :to => "welcome#index"
-#     get "signup", :to => "users/registrations#new"
-#     get "login", :to => "users/sessions#new"
-#     get "logout", :to => "users/sessions#destroy"
-#   end
-
-#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# end
-
 Rails.application.routes.draw do
+  # get 'mypage/index'
+  resources :mypage, :only => [ :index ]
   root 'welcome#index' #これを追記 
 
     devise_scope :user do
