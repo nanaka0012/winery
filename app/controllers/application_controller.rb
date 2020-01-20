0f9a130bdff78bbce6817862872500fae5536374
nanaka0012 <nanaka0012@yahoo.co.jp>
2020-01-20 22:21:14 +0900

    update: header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!
 
     # このアクションを追加
     def after_sign_in_path_for(resource)
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,4 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     @purchases = Purchase.where(user: current_user)
   end
